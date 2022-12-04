bb4d2402e520a58050129d9eede5711106666387
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-04 20:32:59 +0500

    change structure class Vaz

---
+++ lab_2/VAZ.py
@@ -0,0 +1,25 @@
+from Car import Car
+class VAZ(Car):
+    def __init__(self, hp, color, max_speed, model, condition):
+        super().__init__(hp, color, max_speed)
+        self.__model = model
+        self.__condition = condition
+    @property
+    def model(self):
+        return self.__model
+    @property
+    def condition(self):
+        return self.__condition
+    @model.setter
+    def model(self, m):
+        self.__model = m
+    @condition.setter
+    def condition(self, c):
+        self.__condition = c
+    def info(self):
+        print("VAZ")
+        print(f"Hp: {self.hp}")
+        print("Color:" + self.color)
+        print(f"Max speed: {self.max_speed}")
+        print("Model:" + self.__model)
+        print("Condition: " + self.__condition)
