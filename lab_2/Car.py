3d79a51a13ab89e7ea5123b009edc033c4bbe203
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-04 18:27:11 +0500

    constuctor

--- lab_2/Car.py
+++ lab_2/Car.py
@@ -3,28 +3,3 @@ class Car:
         self.__hp = hp
         self.__color = color
         self.__max_speed = max_speed
-    def hp(self):
-        return self.__hp
-    def hp (self, h):
-        self.__hp = h
-    def color(self):
-        return self.__color
-    def color(self, c):
-        self.__color = c
-    def max_speed(self):
-        return self.__max_speed
-
-    def max_speed(self, s):
-        self.__max_speed = s
-
-    def max_speed(self):
-        return self.__max_speed
-
-    def max_speed(self, s):
-        self.__max_speed = s
-
-    def info(self):
-        print("Car")
-        print(f"Hp: {self.__hp}")
-        print("Color:" + self.__color)
-        print(f"Max speed: {self.__max_speed}")
