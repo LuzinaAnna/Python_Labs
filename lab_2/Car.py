261e3e99dd0cd8aa9cb688410a2c58f3cc35a35b
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-04 18:18:57 +0500

    property color

--- lab_2/Car.py
+++ lab_2/Car.py
@@ -3,8 +3,11 @@ class Car:
         self.__hp = hp
         self.__color = color
         self.__max_speed = max_speed
-        def hp(self):
-            return self.__hp
-        def hp(self,h):
-            self.__hp=h
-    pass
+    def hp(self):
+        return self.__hp
+    def hp (self, h):
+        self.__hp = h
+    def color(self):
+        return self.__color
+    def color(self, c):
+        self.__color = c
