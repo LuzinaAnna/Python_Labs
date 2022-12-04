1e6e23a8b8b344924523fec0819fb483b0824306
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-04 20:03:43 +0500

    change structure class Car 2.0

--- lab_2/Car.py
+++ lab_2/Car.py
@@ -3,19 +3,25 @@ class Car:
         self.__hp = hp
         self.__color = color
         self.__max_speed = max_speed
+    @property
     def hp(self):
         return self.__hp
+    @hp.setter
     def hp (self, h):
         if h>0:
           self.__hp = h
         else:
           raise ValueError
+    @property
     def color(self):
         return self.__color
+    @color.setter
     def color(self, c):
         self.__color = c
+    @property
     def max_speed(self):
         return self.__max_speed
+    @max_speed.setter
     def max_speed(self, s):
         if s>0:
           self.__max_speed = s
