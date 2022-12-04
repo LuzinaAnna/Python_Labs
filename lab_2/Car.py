ae6f1247684e62d712b96d663a474d21bef6371b
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-04 17:32:28 +0500

    property

---
+++ lab_2/Car.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, hp, color, max_speed):
+        self.__hp = hp
+        self.__color = color
+        self.__max_speed = max_speed
+        def hp(self):
+            return self.__hp
+
+    pass
