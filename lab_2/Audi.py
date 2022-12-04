fcd8330aa5cf01103f9c523f5ab06212dffcfd85
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-04 20:04:31 +0500

    change structure class Audi

---
+++ lab_2/Audi.py
@@ -0,0 +1,28 @@
+from Car import Car
+class Audi(Car):
+    def __init__(self, hp, color, max_speed, model, wheel_radius):
+        super().__init__(hp, color, max_speed)
+        self.__model = model
+        self.__wheel_radius = wheel_radius
+    @property
+    def model(self):
+        return self.__model
+    @property
+    def wheel_radius(self):
+        return self.__wheel_radius
+    @model.setter
+    def model(self, m):
+        self.__model = m
+    @wheel_radius.setter
+    def wheel_radius(self, r):
+        if r>0:
+            self.__wheel_radius = r
+        else:
+            raise ValueError
+    def info(self):
+        print("Audi")
+        print(f"Hp: {self.hp}")
+        print("Color:" + self.color)
+        print(f"Max speed: {self.max_speed}")
+        print("Model:"+self.__model)
+        print(f"Wheel radius: {self.__wheel_radius}")
