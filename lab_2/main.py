0e4062f6883eeb466b933f3a689b3ead7f2f2d8a
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-06 13:14:54 +0500

    push the directory

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,12 +1,11 @@
-from Car import Car
-from Audi import Audi
-from BMW import BMW
-from Mersedes import Mersedes
-from VAZ import VAZ
+from lab_2.cars.Car import Car
+from lab_2.cars.Audi import Audi
+from lab_2.cars.BMW import BMW
+from lab_2.cars.Mersedes import Mersedes
+from lab_2.cars.VAZ import VAZ
 c = Car(120, "green", 250)
 a = Audi(140, "red", 270, "Q7", 14)
 b = BMW(100, "black", 230, "M3", 1998)
-
 m = Mersedes(150, "white", 290, "GLE Coupe I", "S")
 v = VAZ(70, "cherry", 130, "пятерка","good")
 c.info()
