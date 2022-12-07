67d7c15f8f18c366f520842eb5c2fddd60182801
LuzinaAnna <luzina.ann03@gmail.com>
2022-12-07 13:34:36 +0500

    adding serialization of cars

--- lab_2/main.py
+++ lab_2/main.py
@@ -16,7 +16,7 @@ c = Car(120, "green", 250)
 a = Audi(140, "red", 270, "Q7", 14)
 b = BMW(100, "black", 230, "M3", 1998)
 m = Mersedes(150, "white", 290, "GLE Coupe I", "S")
-v = VAZ(70, "cherry", 130, "пятерка", "good")
+v = VAZ(70, "cherry", 130, "five", "good")
 l = [c, a, b, m, v]
 data = {
     'amount' : len(l),
